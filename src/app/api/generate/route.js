ab4a1fa02516e3e53f5f5aaf8d94a33853b577a1
Shankar Kumar Nanda <shankarkumarnanda@Shankars-MacBook-Air.local>
2024-08-14 11:58:11 +0530

    first commit

---
+++ src/app/api/generate/route.js
@@ -0,0 +1,34 @@
+import { gallary } from '@/data/gallary';
+// import { kv } from '@vercel/kv';
+import { getServerSession } from 'next-auth';
+import { trace } from 'next/dist/trace';
+
+export async function POST(req) {
+  try {
+    const session = await getServerSession({ req });
+
+    if (!session) {
+      return new Response(JSON.stringify({ error: 'Unauthorized' }), { status: 401 });
+    }
+
+    // console.log(kv)
+
+    // const userKey = `rate_limit:${session.user.email}`;
+    // const userRate = await kv.get(userKey);
+
+    // if (userRate && userRate >= 3) {
+    //   return new Response(JSON.stringify({ error: 'Rate limit exceeded! Come after hour!' }), { status: 429 });
+    // }
+
+    // await kv.incr(userKey);
+    // await kv.expire(userKey, 3600);
+
+    const randomImage = gallary[Math.floor(Math.random() * gallary.length) % 14];
+    console.log(randomImage)
+
+    return new Response(JSON.stringify({ image: randomImage.path, alt: randomImage.alt, session }), { status: 200 });
+  } catch (error) {
+    console.error('Error in POST /api/generate:', error);
+    return new Response(JSON.stringify({ error: 'Internal Server Error' }), { status: 500 });
+  }
+}
---
+++ src/data/gallary.js
@@ -0,0 +1,58 @@
+export const gallary = [
+    {
+        path: "/food.jpeg",
+        alt: "image1"
+    },
+    {
+        path: "/catsFood.jpeg",
+        alt: "image2"
+    },
+    {
+        path: "/tiger.jpeg",
+        alt: "image3"
+    },
+    {
+        path: "/racingCars.jpeg",
+        alt: "image4"
+    },
+    {
+        path: "/car.jpeg",
+        alt: "image5"
+    },
+    {
+        path: "/bear.jpeg",
+        alt: "image6"
+    },
+    {
+        path: "/astronaut.jpeg",
+        alt: "image7"
+    },
+    {
+        path: "/AI.jpeg",
+        alt: "image8"
+    },
+    {
+        path: "/women.jpg",
+        alt: "image9"
+    },
+    {
+        path: "/vegetables.jpeg",
+        alt: "image10"
+    },
+    {
+        path: "/cooking.jpeg",
+        alt: "image11"
+    },
+    {
+        path: "/lemon.jpeg",
+        alt: "image12"
+    },
+    {
+        path: "/eating.jpeg",
+        alt: "image13"
+    },
+    {
+        path: "/horses.jpeg",
+        alt: "image14"
+    }
+] 
