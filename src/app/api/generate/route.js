dff41063443e94d04a29bd9b7a905497fa7b6161
Shankar Kumar Nanda <shankarkumarnanda@Shankars-MacBook-Air.local>
2024-08-14 12:03:16 +0530

    Updated the KV FILE

--- src/app/api/generate/route.js
+++ src/app/api/generate/route.js
@@ -1,5 +1,5 @@
 import { gallary } from '@/data/gallary';
-// import { kv } from '@vercel/kv';
+import { kv } from '@vercel/kv';
 import { getServerSession } from 'next-auth';
 import { trace } from 'next/dist/trace';
 
@@ -11,17 +11,17 @@ export async function POST(req) {
       return new Response(JSON.stringify({ error: 'Unauthorized' }), { status: 401 });
     }
 
-    // console.log(kv)
+    console.log(kv)
 
-    // const userKey = `rate_limit:${session.user.email}`;
-    // const userRate = await kv.get(userKey);
+    const userKey = `rate_limit:${session.user.email}`;
+    const userRate = await kv.get(userKey);
 
-    // if (userRate && userRate >= 3) {
-    //   return new Response(JSON.stringify({ error: 'Rate limit exceeded! Come after hour!' }), { status: 429 });
-    // }
+    if (userRate && userRate >= 3) {
+      return new Response(JSON.stringify({ error: 'Rate limit exceeded! Come after hour!' }), { status: 429 });
+    }
 
-    // await kv.incr(userKey);
-    // await kv.expire(userKey, 3600);
+    await kv.incr(userKey);
+    await kv.expire(userKey, 3600);
 
     const randomImage = gallary[Math.floor(Math.random() * gallary.length) % 14];
     console.log(randomImage)
